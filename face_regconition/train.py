b0912a1a03d00ce9634e0fed7f0bd730e9d3c562
kiet.do <kiet.do@pirago.work>
2022-02-17 14:46:05 +0700

    feat: model train

--- face_regconition/train.py
+++ face_regconition/train.py
@@ -13,18 +13,32 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 print(train_images.shape)
 
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i])
-    # The CIFAR labels happen to be arrays, 
-    # which is why you need the extra index
-    plt.xlabel(class_names[train_labels[i]])
-plt.show()
+# plt.figure(figsize=(7,7))
+# for i in range(25):
+#     plt.subplot(5,5,i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(train_images[i])
+#     # The CIFAR labels happen to be arrays, 
+#     # which is why you need the extra index
+#     plt.xlabel(class_names[train_labels[i]])
+# plt.show()
 
 model = models.Sequential([
-    layers.Flatten()
+    layers.Flatten(input_shape = (28, 28)),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(10)
 ])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=10)
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+print('\nTest accuracy:', test_acc)
+
+tf.keras.save_model(model)
