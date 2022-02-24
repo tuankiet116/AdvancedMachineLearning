89c38ad5fc0521ae17a2e60da6dcb998fd1bf6d3
tuankiet116 <kietdat1612000@gmail.com>
2022-02-24 17:09:05 +0700

    feat: system login

--- src/login.py
+++ src/login.py
@@ -1,36 +1,78 @@
 from base64 import b64decode
-import face_recognition as fr
-import time
 import os
-import pickle
+import cv2
+import tensorflow as tf
+from tensorflow.keras.layers import Layer
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class L1Dist(Layer):
+    def __init__(self, **kwargs):
+        super().__init__()
+       
+    # Tính toán khoảng cách
+    def call(self, input_embedding, validation_embedding):
+        print(tf.math.abs(input_embedding - validation_embedding))
+        return tf.math.abs(input_embedding - validation_embedding)
+    
+def preprocess_twin(input_img, validation_img, label):
+    return(preprocess(input_img), preprocess(validation_img), label)
+
+def preprocess(file_path):
+    # Đọc ảnh 
+    byte_img = tf.io.read_file(file_path)
+    # Load ảnh 
+    img = tf.io.decode_jpeg(byte_img)
+    
+    # Tiền xử lý chuyển ảnh về 100x100x3
+    img = tf.image.resize(img, (100,100))
+    # Đưa ảnh về khoảng 0-1
+    img = img / 255.0
+
+    # Return image
+    return img
+
+def verify(model, detection_threshold, verification_threshold, verify_path):
+    results = []
+    input_img = preprocess(os.path.join('application_data', 'input_image', 'input_image.jpg'))
+    for image in os.listdir(verify_path):
+        print(image)
+        validation_img = preprocess(os.path.join(verify_path, image))
+        # Dự đoán
+        result = model.predict(list(np.expand_dims([input_img, validation_img], axis=1)))
+        results.append(result)
+    
+    # Ngưỡng nhận diện: Chỉ số dự đoán lớn hơn ngưỡng dự đoán 
+    detection = np.sum(np.array(results) > detection_threshold)
+    # Ngưỡng Verified: Tỷ lệ dự đoán ảnh đúng(positive)  / Tổng số lượng ảnh của người nhận diện (positive) 
+    verification = detection / len(os.listdir(os.path.join('application_data', 'verification_images'))) 
+    verified = verification > verification_threshold
+    
+    return results, verified
 
 def login_check(email, image):
-    face_match = 0
+    verification_path = os.path.join('application_data', 'verification_images', email)
+    input_path = os.path.join('application_data', 'input_image', 'input_image.jpg')
+    
     header, encoded = image.split(",", 1)
-    file_new = str(time.time_ns())
-    file_exist = str(time.time_ns())
-
-    with open(file_new + ".png", "wb") as f:
+    with open(input_path, "wb") as f:
         f.write(b64decode(encoded))
-
-    data = pickle.loads(open("data.pickle", "rb").read())
-    with open(file_exist + ".png", "wb") as f:
-        f.write(b64decode(data[email]))
+    
+    siamese_model = tf.keras.models.load_model('models/siamesemodelv2.h5', 
+                                   custom_objects={'L1Dist':L1Dist, 'BinaryCrossentropy':tf.losses.BinaryCrossentropy})
     
     try:
         try:
-            got_image = fr.load_image_file(file_new + ".png")
-            existing_image = fr.load_image_file(file_exist + ".png")
+            results, verified = verify(siamese_model, 0.5, 0.5, verification_path)
+            print(results)
+            if(verified == True):
+                return "Successfully Logged in!"
+            else: 
+                return "Login Fail"
         except Exception as e:
             print(e.__cause__)
             return "Data does not exist!"
-        got_image_facialfeatures = fr.face_encodings(got_image)[0]
-        existing_image_facialfeatures = fr.face_encodings(existing_image)[0]
-        results = fr.compare_faces([existing_image_facialfeatures], got_image_facialfeatures)
-        if(results[0]):
-            return "Successfully Logged in!"
-        else:
-            return "Failed to Log in!"
     except Exception as e:
         print(e.__cause__)
         return "Image not clear! Please try again!"
--- src/static/js/main.js
+++ src/static/js/main.js
@@ -20,14 +20,39 @@ var constraints = {
     },
     audio: false
 };
-navigator.mediaDevices.getUserMedia(constraints)
-    .then(function(stream) {
+
+var selectDevice = document.getElementById('select-device');
+checkDevices();
+makeVideo();
+
+async function checkDevices() {
+    var devices = await navigator.mediaDevices.enumerateDevices()
+    var options = '';
+    devices.forEach(element => {
+        if (element.deviceId != "") {
+            options += `<option value="${element.deviceId}">${element.label}</option>`
+        }
+    });
+    selectDevice.innerHTML = options;
+}
+
+selectDevice.addEventListener('change', function () {
+    localMediaStream.getTracks()[0].stop();
+    constraints.video.deviceId = this.value;
+    makeVideo();
+});
+
+async function makeVideo() {
+    navigator.mediaDevices.getUserMedia(constraints)
+    .then(function (stream) {
         video.srcObject = stream;
         localMediaStream = stream;
     })
-    .catch(function(error) {
+    .catch(function (error) {
         console.log(error);
     });
+}
+
 function login() {
     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
     var dataURL = canvas.toDataURL('image/png');
